69e648672e4ace4b6ad16372de07bd4f254cdba3
Pixel Gui <guillaumethomart@gmail.com>
2024-04-18 19:30:48 +0200

    refacto main function

--- script.js
+++ script.js
@@ -2,41 +2,23 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const countdown = (number) => {
+  console.log(number);
+};
+
 const decimalToBinary = (input) => {
-// Reçoit les entrées
-  const inputs = [];
-// Stock les résultats des divisions
-  const quotients = [];
-// Stock le reste de la division si le resultat n'est pas un entier 
-  const remainders = [];
+  let binary = "";
 
   if (input === 0) {
-    result.innerText = "0";
-    return;
+    binary = "0";
   }
 
-while (input > 0) {
-// Ici divise l'entrée par 2 et arrondi son résultat à un son entier inférieur
-  const quotient = Math.floor(input / 2);
-// Ici on récupère le reste de la division grace à l'opérateur modulo "%". Retournera 1 si impair, 0 si pair
-  const remainder = input % 2;
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
-// On ajoute les valeurs calculées aux tableaux plus haut
-  inputs.push(input);
-  quotients.push(quotient);
-  remainders.push(remainder);
-
-  // Sécurise la boucle pour quelle arrive à 0.
-  input = quotient;
-  // input = 0; Pour sécuriser une boucle infinie pendant la mise en place de la function
-}
-
-console.log("Inputs: ", inputs);
-console.log("Quotients: ", quotients);
-console.log("Remainders: ", remainders);
-
-// Affiche le résultat en base-2. Le binaire commançant par la fin de la division, on le reverse.
-result.innerText = remainders.reverse().join("");
+  result.innerText = binary;
 };
 
 // Clean de l'input user
